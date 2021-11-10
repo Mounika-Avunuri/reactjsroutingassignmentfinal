6b600ca71c534ae8dac7d14db82760c4533aece6
mounuikavedhanshi <89015579+mounuikavedhanshi@users.noreply.github.com>
2021-11-10 13:12:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="main">
       <Router>
-        <Content />
+        <Navbar />
         <Switch >
           <Route path='/Unmounting'>
             <Unmounting />
