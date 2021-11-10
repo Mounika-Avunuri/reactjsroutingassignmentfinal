d927056d2eb68b01775fe645547a38e3ffbb377a
mounuikavedhanshi <89015579+mounuikavedhanshi@users.noreply.github.com>
2021-11-10 13:15:30 +0530

    Update Unmounting.js

--- src/components/Unmounting.js
+++ src/components/Unmounting.js
@@ -3,12 +3,15 @@ import React from 'react'
 function Unmounting() {
     return (
         <div>
+        <center>
+        <h1>Unmounting</h1>
             <p>
                 It is the final phase of the react component lifecycle. It is called when a component instance is destroyed and unmounted from the DOM. This phase contains only one method and is given below.
 
                 *componentWillUnmount()
                 This method is invoked immediately before a component is destroyed and unmounted permanently. It performs any necessary cleanup related task such as invalidating timers, event listener, canceling network requests, or cleaning up DOM elements. If a component instance is unmounted, you cannot mount it again.
             </p>
+    </center>
         </div>
     )
 }
